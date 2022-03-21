77775996aa1c09618c58132c064de168c8aa68f9
laurabiggins <laurabiggins10@gmail.com>
2022-03-21 10:36:39 +0000

    Changed density plot background to light theme.

--- R/custom_density_plot.R
+++ R/custom_density_plot.R
@@ -6,7 +6,8 @@ density_toothGrowth <- function() {
     geom_density(alpha = 0.7, size = 2) +
     scale_color_manual(values = c("#1f9492", "#4322a3")) +
     scale_fill_manual(values = c("#1f9492", "#4322a3")) +
-    xlim(-5, 45)
+    xlim(-5, 45) +
+    theme_light()
   
 }  
   
