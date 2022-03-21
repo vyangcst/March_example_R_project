3d26ce4e7a44c01d449e40333e669de8aa0ff57b
laurabiggins <laurabiggins10@gmail.com>
2022-03-21 10:33:17 +0000

    Added some documentation for scatterplot

--- R/custom_scatterplot.R
+++ R/custom_scatterplot.R
@@ -1,3 +1,16 @@
+#' custom_scatter plot
+#'
+#' Function to create a scatterplot of the iris dataset.
+#'   
+#' @param x_var variable to plot on x axis
+#' @param y_var variable to plot on y axis
+#'
+#' @return ggplot object
+#' @export
+#'
+#' @examples
+#' custom_scatter("Sepal.Width", "Sepal.Length")
+
 custom_scatter <- function(x_var, y_var){
   ggplot2::ggplot(
     data = iris, 
